d9955940ccfd9bfb2be8a8ca2add61ac8ff36344
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-02-16 15:36:26 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -13,3 +13,22 @@ function calculateHourlyWage(salary, hoursPerWeek) {
 
 console.log(`Hourly Wages: $${calculateHourlyWage(52000, 40)}`)//Output: Hourly Wages: $25.00
 console.log(`Hourly Wages: $${calculateHourlyWage(75000, 35)}`)//Output: Hourly Wage: $41.21
+
+//Task 3 Arrow Function
+let calculateLoyaltyDiscount = (amount, years) => {
+    let discount; 
+    if (years >= 5) { 
+        discount = amount * 0.15; // 15% discount
+    } else if (years >= 3) { 
+        discount = amount * 0.1; // 10% discount 
+    } else if (years < 3) {
+        discount = amount * 0.05; // 5% discount 
+    }
+    return (amount - discount).toFixed(2); 
+    // subtracting the amount in the test data by the calculated discount 
+};
+
+console.log(`Discounted Price: $${calculateLoyaltyDiscount(100, 6)}`); // Expected output: "Discounted Price: $85.00"
+console.log(`Discounted Price: $${calculateLoyaltyDiscount(200, 2)}`); // Expected output: "Discounted Price: $190.00"
+
+//Task 4 Parameters and Arguments
